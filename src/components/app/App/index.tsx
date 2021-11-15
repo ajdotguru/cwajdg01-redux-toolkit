083a31ef2ba264ce920e5d22b234221d0aa9251d
ajdotguru <ajfsdeveloper@gmail.com>
2021-11-15 19:09:25 +0530

    REDUX_DEMO: add react router dom

--- src/components/app/App/index.tsx
+++ src/components/app/App/index.tsx
@@ -1,9 +1,18 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import { Container } from '@mui/material';
 import { Provider } from 'react-redux';
 import store from 'redux-store/store';
-import { Home } from 'components/pages';
+import { AppRoutes } from 'components/app';
+import { theme } from './theme';
 
 export const App = () => (
 	<Provider store={store}>
-		<Home />
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<Container maxWidth="lg">
+				<AppRoutes />
+			</Container>
+		</ThemeProvider>
 	</Provider>
 );
